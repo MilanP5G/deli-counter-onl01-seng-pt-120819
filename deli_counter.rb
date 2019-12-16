6857913e2f80b2622eef1a27dc20a29c87399e58
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 13:51:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,12 @@ katz_deli = []
 
 def line(katz_deli)
   
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
+  if katz_deli.length >= 1
     other_deli = "The line is currently:"
     katz_deli.each.with_index(1) do |customer, num| 
       other_deli << " #{num}. #{customer}"
+  else katz_deli.length == 0
+    puts "The line is currently empty."
   end
     puts other_deli
   end
