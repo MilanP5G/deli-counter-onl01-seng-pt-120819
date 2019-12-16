a24991bf52dad2b8da2dffb9a9e733fb4de7631b
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 14:07:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,10 +17,10 @@ def line(katz_deli)
   
 end 
 
-def take_a_number(array, name)
+def take_a_number(array)
   
   katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   
 end
 
