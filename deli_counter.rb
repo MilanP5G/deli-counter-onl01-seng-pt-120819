a8053aed89a1b123e2e6a69f97e5a3a79f8be78d
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 13:44:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,13 +5,13 @@ katz_deli = []
 
 def line(array)
   
-  if katz_deli == 0
-    puts "The line is currently empty."
-  else katz_deli >= 1
+  if katz_deli >= 1
     other_deli = ["The line is currently: "]
     counter = 1
     katz_deli.each {|number, name| other_deli << "#{number}. #{name}"}
     counter += 1
+  else katz_deli == 0
+    puts "The line is currently empty."
   end
   
 end 
