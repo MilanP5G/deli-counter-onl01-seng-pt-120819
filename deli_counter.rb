2d3fc929d532a5c3f85f22e1eb9971dbc8f5ece8
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 13:36:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+
+katz_deli = []
+
+def line(array)
+  
+  if array.length >= 1
+    counter = 1
+    array.each {|counter, name| puts "The line is currently: #{counter}. #{name}"}
+    counter += 1
+  else 
+    puts "The line is currently empty."
+  end
+  
+end 
+  
+
+def take_a_number(array, name)
+
+end
+
+def now_serving(array)
+  
+end
