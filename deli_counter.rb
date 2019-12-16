d461fd31a4d2cf0ca41cc2ed1e25486d63a5db85
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 14:03:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,8 +26,11 @@ end
 
 def now_serving(array)
   
-  katz_deli << name
-  puts "Currently serving #{name}"
-    
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
+  end
 
 end
