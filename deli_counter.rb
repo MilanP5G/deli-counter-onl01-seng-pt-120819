80c6ab3f88b5cfdb88318add8977c40a28f4f8c7
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 13:51:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    other_deli = "The line is currently: "
+    other_deli = "The line is currently:"
     katz_deli.each.with_index(1) do |customer, num| 
       other_deli << " #{num}. #{customer}"
   end
